ac296b33978c78b6647e5d3e2ee8eb6485776007
dimka77783 <dimka77783@mai.ru>
2022-01-26 21:35:49 +0300

    добавлен HTML к странице

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
-
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
-
-# Create your views here.
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
