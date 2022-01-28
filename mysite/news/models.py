8db8f8603041942ae1f8036fb1116f092e342f77
dimka77783 <dimka77783@mai.ru>
2022-01-28 19:13:53 +0300

    1

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликован')
 
     def __str__(self):
         return self.title
